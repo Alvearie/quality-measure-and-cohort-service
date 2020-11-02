eefe28ff3746a85241d5116ed882384a5ea5b4e6
Corey Sanders <corey.thecolonel@gmail.com>
2020-11-02 15:43:25 -0500

    Add support for the IBM FHIR datasource ID header

--- cohort-engine/src/test/java/com/ibm/cohort/engine/IBMFhirServerConfigTest.java
+++ cohort-engine/src/test/java/com/ibm/cohort/engine/IBMFhirServerConfigTest.java
@@ -7,7 +7,10 @@
 package com.ibm.cohort.engine;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -22,6 +25,8 @@ public class IBMFhirServerConfigTest {
 		config.setPassword("change-password");
 		config.setTenantId("default");
 		config.setTenantIdHeader("X-NOT-REAL");
+		config.setDataSourceId("non-default");
+		config.setDataSourceIdHeader("X-MY-DSID");
 		
 		ObjectMapper om = new ObjectMapper();
 		String deflated = om.writeValueAsString( config );
@@ -34,5 +39,31 @@ public class IBMFhirServerConfigTest {
 		assertEquals( config.getPassword(), inflated.getPassword());
 		assertEquals( config.getTenantId(), inflated.getTenantId());
 		assertEquals( config.getTenantIdHeader(), inflated.getTenantIdHeader());
+		assertEquals( config.getDataSourceId(), inflated.getDataSourceId());
+		assertEquals( config.getDataSourceIdHeader(), inflated.getDataSourceIdHeader());
+	}
+	
+	@Test
+	public void testHeadersSetWhenNotNull() {
+		IBMFhirServerConfig config = new IBMFhirServerConfig();
+		config.setEndpoint("https://localhost:9443/fhir-server/api/v4");
+		config.setTenantId("default");
+		config.setTenantIdHeader("X-NOT-REAL");
+		config.setDataSourceId("non-default");
+		config.setDataSourceIdHeader("X-MY-DSID");
+		
+		Map<String,String> headers = config.getAdditionalHeaders();
+		assertEquals(headers.get("X-NOT-REAL"), "default");
+		assertEquals(headers.get("X-MY-DSID"), "non-default");
+	}
+	
+	@Test
+	public void testHeadersNotSetWhenNull() {
+		IBMFhirServerConfig config = new IBMFhirServerConfig();
+		config.setEndpoint("https://localhost:9443/fhir-server/api/v4");
+		
+		Map<String,String> headers = config.getAdditionalHeaders();
+		assertFalse(headers.containsKey("X-NOT-REAL"));
+		assertFalse(headers.containsKey("X-MY-DSID"));
 	}
 }
