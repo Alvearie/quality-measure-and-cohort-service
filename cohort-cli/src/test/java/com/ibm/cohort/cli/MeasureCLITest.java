a3d3104c38f24f77f1c7eca7571314627852751a
matthew.sargent <matthew.sargent@ibm.com>
2020-12-03 15:59:07 -0500

    Add cohort cli tests

--- cohort-cli/src/test/java/com/ibm/cohort/cli/MeasureCLITest.java
+++ cohort-cli/src/test/java/com/ibm/cohort/cli/MeasureCLITest.java
@@ -70,6 +70,79 @@ public class MeasureCLITest extends BaseMeasureTest {
 		String[] lines = output.split(System.getProperty("line.separator"));
 		assertEquals( output, 2, lines.length );
 	}
+
+	@Test
+	public void testCohortMeasuresMultiplePatients() throws Exception {
+		mockFhirResourceRetrieval("/metadata", getCapabilityStatement());
+
+		Patient patient1 = getPatient("123", AdministrativeGender.MALE, "1592-14-03");
+		mockFhirResourceRetrieval(patient1);
+
+		Patient patient2 = getPatient("234", AdministrativeGender.MALE, "1592-14-04");
+		mockFhirResourceRetrieval(patient2);
+
+		Patient patient3 = getPatient("888", AdministrativeGender.FEMALE, "1592-14-05");
+		mockFhirResourceRetrieval(patient3);
+
+		Library library1 = mockLibraryRetrieval("Test", "cql/basic/test.cql");
+
+		Measure measure = getCohortMeasure("Test", library1, "Male");
+		mockFhirResourceRetrieval(measure);
+
+		File tmpFile = new File("target/fhir-stub.json");
+		ObjectMapper om = new ObjectMapper();
+		try (Writer w = new FileWriter(tmpFile)) {
+			w.write(om.writeValueAsString(getFhirServerConfig()));
+		}
+
+		File tmpResourceParametersFile = createTmpParametersFileFromContents(
+				"{\"measureParameters\":[{\"measureId\":\"" + measure.getId() + "\"}," + 
+						"{\"measureId\":\"" + measure.getId() + "\",\"parameters\":[" + createParameterString("p1", "integer", "10")+ "]}]}");
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(baos);
+		try {
+			MeasureCLI cli = new MeasureCLI();
+			cli.runWithArgs(new String[] {
+					"-d", tmpFile.getAbsolutePath(),
+					"-r", tmpResourceParametersFile.getAbsolutePath(),
+					"-c", patient1.getId(),
+					"-c", patient2.getId(),
+					"-c", patient3.getId()
+			}, out);
+		} finally {
+			tmpFile.delete();
+			tmpResourceParametersFile.delete();
+		}
+
+		String output = new String(baos.toByteArray());
+		String[] lines = output.split(System.getProperty("line.separator"));
+		// First two patients create 7 lines of output each (1 for start of patient context, 3 per measure result).
+		// Last patient only creates 2 lines (1 for start of patient context, 1 for line divider since there is no measure report returned).
+		assertEquals( output, 16, lines.length );
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingResourceParametersFile() throws Exception {
+		File tmpFile = new File("target/fhir-stub.json");
+		ObjectMapper om = new ObjectMapper();
+		try (Writer w = new FileWriter(tmpFile)) {
+			w.write(om.writeValueAsString(getFhirServerConfig()));
+		}
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(baos);
+		try {
+			MeasureCLI cli = new MeasureCLI();
+			cli.runWithArgs(new String[] {
+					"-d", tmpFile.getAbsolutePath(),
+					"-r", "target/garbageFilepciwebocwe8293ivsohvb",
+					"-c", "p1"
+			}, out);
+		} finally {
+			tmpFile.delete();
+		}
+	}
 	
 	@Test
 	public void testProportionRatioSinglePatient() throws Exception {
@@ -287,6 +360,10 @@ public class MeasureCLITest extends BaseMeasureTest {
 	
 	private File createTmpParametersFileForSingleMeasure(String measureId) throws IOException {
 		return createTmpParametersFileFromContents("{\"measureParameters\":[{\"measureId\":\"" + measureId + "\"}]}");
+	}
+	
+	private String createParameterString(String name, String type, String value) {
+		return "{\"name\":\"" + name + "\",\"type\":\"" + type + "\",\"value\":\"" + value +"\"}";
 	}
 	
 	private File createTmpParametersFileFromContents(String contents) throws IOException {
