054fb628eec35fafbf2d7199a7d36d1ec6f9dff3
matthew.sargent <matthew.sargent@ibm.com>
2020-12-04 12:27:14 -0500

    Copyright

--- cohort-cli/src/main/java/com/ibm/cohort/cli/input/MeasureIdWithParameters.java
+++ cohort-cli/src/main/java/com/ibm/cohort/cli/input/MeasureIdWithParameters.java
@@ -1,3 +1,8 @@
+/*
+ * (C) Copyright IBM Corp. 2020, 2020
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package com.ibm.cohort.cli.input;
 
 import static com.ibm.cohort.cli.input.InputUtil.isNullOrEmpty;
