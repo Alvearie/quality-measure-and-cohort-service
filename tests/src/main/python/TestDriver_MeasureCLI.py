e323477d5aab5c0a87ede861ac95aa245863b205
matthew.sargent <matthew.sargent@ibm.com>
2021-01-05 18:25:13 -0500

    Change parameter name

--- tests/src/main/python/TestDriver_MeasureCLI.py
+++ tests/src/main/python/TestDriver_MeasureCLI.py
@@ -20,17 +20,17 @@ def setup():
         data = json.load(f)
         testValues = data['tests']
         for testValue in testValues.values():
-            tests.append((testValue['measureConfigurationFile'], testValue['resource'], testValue['params'], testValue['targets'], testValue['response'], testValue['measureServer']))
+            tests.append((testValue['jsonMeasureConfigurationFile'], testValue['resource'], testValue['params'], testValue['targets'], testValue['response'], testValue['measureServer']))
     return tests
  
 class Test(object):
 
-    @pytest.mark.parametrize("measureConfigurationFile, resource, params, targets, output, measureServer",setup())
-    def test1(self, measureConfigurationFile, resource, params, targets, output, measureServer):
-        self.execute(measureConfigurationFile, resource, params, targets, output, measureServer)
+    @pytest.mark.parametrize("jsonMeasureConfigurationFile, resource, params, targets, output, measureServer",setup())
+    def test1(self, jsonMeasureConfigurationFile, resource, params, targets, output, measureServer):
+        self.execute(jsonMeasureConfigurationFile, resource, params, targets, output, measureServer)
 
     # Execute submits a query and validates the return.
-    def execute(self, measureConfigurationFile, resource, params, targets, output, measureServer):
+    def execute(self, jsonMeasureConfigurationFile, resource, params, targets, output, measureServer):
         o = output.split('\n')
         callDetails = ["java", "-Xms1G", "-Xmx1G", "-Djavax.net.ssl.trustStore="+os.environ["TRUSTSTORE"], "-Djavax.net.ssl.trustStorePassword="+os.environ["TRUSTSTORE_PASSWORD"], "-Djavax.net.ssl.trustStoreType="+os.environ["TRUSTSTORE_TYPE"], "-classpath", jar, "com.ibm.cohort.cli.MeasureCLI"]
         if os.environ['DATA_FHIR_SERVER_DETAILS']:
@@ -39,9 +39,9 @@ class Test(object):
         if os.environ['TERM_FHIR_SERVER_DETAILS']:
             callDetails.append("-t")
             callDetails.append(os.environ['TERM_FHIR_SERVER_DETAILS'])
-        if measureConfigurationFile:
-            callDetails.append("-e")
-            callDetails.append(measureConfigurationFile)
+        if jsonMeasureConfigurationFile:
+            callDetails.append("-j")
+            callDetails.append(jsonMeasureConfigurationFile)
         if resource:
             callDetails.append("-r")
             callDetails.append(resource)
