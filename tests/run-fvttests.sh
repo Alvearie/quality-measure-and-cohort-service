1e52b9d811c50265a01deb0ad05ae628c23d08d7
tabishop <tabishop@us.ibm.com>
2021-01-12 11:51:07 -0600

    update pod name related fields

--- tests/run-fvttests.sh
+++ tests/run-fvttests.sh
@@ -5,7 +5,7 @@ failing=0
 output=""
 
 #podname=$(kubectl get pods --namespace "${CLUSTER_NAMESPACE}" | grep -i "${APP_NAME}-${GIT_BRANCH}-${CHART_NAME}" | grep Running | cut -d " " -f 1 | head -n1)
-podname=$(kubectl get pods --namespace "${CLUSTER_NAMESPACE}" | grep -i "${APP_NAME}-tenant-id" | grep Running | cut -d " " -f 1 | head -n1)
+podname=$(kubectl get pods --namespace "${CLUSTER_NAMESPACE}" | grep -i "${APP_NAME}" | grep Running | cut -d " " -f 1 | head -n1)
 
 echo "### serviceState ###"
 test1=$(kubectl exec --namespace="${CLUSTER_NAMESPACE}" $podname -- curl http://localhost:9080/services/cohort/api/v1/status?liveness_check=false)
