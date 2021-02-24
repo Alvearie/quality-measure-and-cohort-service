9945aa81956cc5c972e537a1c781dce1f088edf8
shelley.murphy <mshelley@us.ibm.com>
2021-02-24 09:28:01 -0500

    Removing unused import

--- fhir-resource-tooling/src/main/java/com/ibm/cohort/tooling/fhir/VSACValueSetImporter.java
+++ fhir-resource-tooling/src/main/java/com/ibm/cohort/tooling/fhir/VSACValueSetImporter.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
