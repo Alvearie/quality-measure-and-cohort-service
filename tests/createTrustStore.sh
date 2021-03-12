25557f20b872f087864409428cad7456b1b5134c
tabishop <tabishop@us.ibm.com>
2021-03-12 14:13:32 -0600

    allow override of FHIR_CLUSTER_NAMESPACE from keyProtect

--- tests/createTrustStore.sh
+++ tests/createTrustStore.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo | openssl s_client -connect fhir-internal.${FHIR_CLUSTER_NAMESPACE}.svc:9443 -showcerts 2>/dev/null | openssl x509 > fhir-server.cer
+# check if FHIR_CLUSTER_NAMESPACE has been set in the toolchain
+# properties and use it if it is there, assume fhir will be
+# running in the same namespace
+if [[ -z "${FHIR_CLUSTER_NAMESPACE}" ]]
+then
+  FHIR_CLUSTER_NAMESPACE_VAL=${CLUSTER_NAMESPACE}
+else
+  FHIR_CLUSTER_NAMESPACE_VAL=${FHIR_CLUSTER_NAMESPACE}
+fi
+
+echo FHIR_CLUSTER_NAMESPACE_VAL=${FHIR_CLUSTER_NAMESPACE_VAL}
+
+echo | openssl s_client -connect fhir-internal.${FHIR_CLUSTER_NAMESPACE_VAL}.svc:9443 -showcerts 2>/dev/null | openssl x509 > fhir-server.cer
 
 keytool -noprompt -importcert -keystore trustStore.pkcs12 -storepass ${TRUSTSTORE_PASSWORD} -alias fhir-server -storetype pkcs12 < fhir-server.cer
 
