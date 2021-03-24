18ce607396bdcce0aa58e2a48cf23b4cf7f6be71
matthew.sargent <matthew.sargent@ibm.com>
2021-03-24 17:37:09 -0400

    Loop  in bash script; lower threshold

--- tests/run-performancetests.sh
+++ tests/run-performancetests.sh
@@ -18,6 +18,26 @@ populateTaurusYaml() {
   -e "/DATA_FHIR_SERVER_DETAILS/s|:.*$|: \"${TESTFVT_TENANT}\"|" ${yamlfile}
 }
 
+checkForTestResults() {
+  resultsDirectory=$1
+  resultsFile=$2
+  sleepTime=$3
+  remainingRetries=$4
+
+  localFileToCheck="${resultsDirectory}/${resultsFile}"
+
+  kubectl -n ${CLUSTER_NAMESPACE} cp "${POD_NAME}:/bzt-configs/tests/results/${resultsFile}" "${localFileToCheck}"
+
+  while [[ ! -f "${localFileToCheck}" ]] && [[ remainingRetries -gt 0 ]];
+  do
+    echo "${localFileToCheck} not found. Attempting to retrieve from test pod."
+    kubectl -n ${CLUSTER_NAMESPACE} cp "${POD_NAME}:/bzt-configs/tests/results/${resultsFile}" "${localFileToCheck}"
+    remainingRetries=$((remainingRetries-1))
+    echo "Sleeping for ${sleepTime} seconds and retrying...${remainingRetries} retries remaining."
+    sleep ${sleepTime}
+  done
+}
+
 . tests/setupEnvironmentVariables.sh
 
 POD_NAME=engine-cohort-ct-perf-test
@@ -30,9 +50,8 @@ kubectl -n ${CLUSTER_NAMESPACE} cp ${SCENARIOS_DIR}/performance/performanceCTSce
 runTest "/bzt-configs/tests/scenarios/performance/performanceCTScenarios.yaml"
 
 PERF_REG_RESULTS="${OUTPUT_DIR}/ct-perf-results"
-echo "Copying test results xml files to ${PERF_REG_RESULTS}"
 mkdir -p ${PERF_REG_RESULTS}
-kubectl -n ${CLUSTER_NAMESPACE} cp "${POD_NAME}:/bzt-configs/tests/results" "${PERF_REG_RESULTS}"
+checkForTestResults "${PERF_REG_RESULTS}" "performanceCTTests.xml" 60 30
 
 # First Check to see if python3 is available for use with xmlCombiner.py script. If not first install python3.8 using yum.
 pythonBinary=python3
