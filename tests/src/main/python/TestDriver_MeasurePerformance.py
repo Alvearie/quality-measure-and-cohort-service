8c01f81fed8ca66002dfee6ea5d319e22c270229
matthew.sargent <matthew.sargent@ibm.com>
2021-03-25 09:44:35 -0400

    add verification to perf python

--- tests/src/main/python/TestDriver_MeasurePerformance.py
+++ tests/src/main/python/TestDriver_MeasurePerformance.py
@@ -26,7 +26,7 @@ class Test(object):
     def test(self, jsonMeasureConfigurationFile, targets, testName):
         self.execute(jsonMeasureConfigurationFile, targets)
 
-# Execute submits a query and waits for the subprocess to complete
+    # Execute submits a query and waits for the subprocess to complete
     def execute(self, jsonMeasureConfigurationFile, targets):
         callDetails = ["java", "-Xms1G", "-Xmx1G", "-Djavax.net.ssl.trustStore="+os.environ["TRUSTSTORE"], "-Djavax.net.ssl.trustStorePassword="+os.environ["TRUSTSTORE_PASSWORD"], "-Djavax.net.ssl.trustStoreType="+os.environ["TRUSTSTORE_TYPE"], "-classpath", jar, "com.ibm.cohort.cli.MeasureCLI"]
         if os.getenv('DATA_FHIR_SERVER_DETAILS'):
@@ -46,7 +46,17 @@ class Test(object):
             callDetails.append(val)
         print("callDetails: " + " ".join(callDetails))
         process = subprocess.Popen(callDetails, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        
-        # Print to stdout for debugging purposes, but assert nothing for the performance tests
+
+        decodedOutput = set()
         for line in process.stdout:
             print(line)
+            decodedOutput.add(line.decode('utf-8'))
+
+        for expectedOutput in self.createExpectedOutput(targets):
+            assert expectedOutput in decodedOutput, 'Output did not contain: ' + expectedOutput
+
+    def createExpectedOutput(self, targets):
+        returnValue = []
+        for target in targets:
+            returnValue.append("Evaluating: " + target)
+        return returnValue
