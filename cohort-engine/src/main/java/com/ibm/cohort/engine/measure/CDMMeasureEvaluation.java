e3b764f446a099a1906045896028d8ffed9df9f1
matthew.sargent <matthew.sargent@ibm.com>
2021-03-31 17:11:58 -0400

    Add comment

--- cohort-engine/src/main/java/com/ibm/cohort/engine/measure/CDMMeasureEvaluation.java
+++ cohort-engine/src/main/java/com/ibm/cohort/engine/measure/CDMMeasureEvaluation.java
@@ -197,7 +197,21 @@ public class CDMMeasureEvaluation {
 			}
 		}
 	}
-	
+
+	/**
+	 * Set the measure reference on a MeasureReport to the id of a given Measure.
+	 * Attempt to normalize the id to just the portion after "Measure/" if possible
+	 *
+	 * Examples:
+	 *   Measure/id1234
+	 *   Measure/id1234/_history/10
+	 *
+	 * If the id does not contain "Measure/", then the report measure is set to the
+	 * full id of the measure.
+	 *
+	 * @param report MeasureReport on which to set the measure reference
+	 * @param measure Measure providing the id to normalize and set on the report
+	 */
 	protected static void setReportMeasureToMeasureId(MeasureReport report, Measure measure) {
 		int startOfId = measure.getId().indexOf("Measure/");
 
