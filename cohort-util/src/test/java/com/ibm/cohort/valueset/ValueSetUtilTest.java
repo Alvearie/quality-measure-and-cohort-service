6312ef5f559123294da3731fc4791cb29843aac2
Jill Doty <jdoty@us.ibm.com>
2021-04-06 15:03:41 -0400

    Missed merge

--- cohort-util/src/test/java/com/ibm/cohort/valueset/ValueSetUtilTest.java
+++ cohort-util/src/test/java/com/ibm/cohort/valueset/ValueSetUtilTest.java
@@ -20,10 +20,7 @@ import org.hl7.fhir.r4.model.ValueSet;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-<<<<<<< HEAD
 
-=======
->>>>>>> Cleanup so it can build
 
 public class ValueSetUtilTest {
 
