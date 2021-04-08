73d6b8ae046194a07ceb8a6c944389d72a72e824
matthew.sargent <matthew.sargent@ibm.com>
2021-04-08 11:47:42 -0400

    Number format

--- cohort-engine/src/test/java/com/ibm/cohort/engine/measure/FhirClientTimeoutTest.java
+++ cohort-engine/src/test/java/com/ibm/cohort/engine/measure/FhirClientTimeoutTest.java
@@ -17,9 +17,9 @@ import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 public class FhirClientTimeoutTest extends BaseFhirTest {
 
 	private static final String PATIENT_ID = "TimeoutTest-PatientId";
-	private static final int PATIENT_RETRIEVAL_DELAY_MILLIS = 3 * 1000;
-	private static final int CONFIG_TIMEOUT_MILLIS = 1000;
-	private static final int CONFIG_NO_TIMEOUT_MILLIS = 20 * 1000;
+	private static final int PATIENT_RETRIEVAL_DELAY_MILLIS = 3_000;
+	private static final int CONFIG_TIMEOUT_MILLIS = 1_000;
+	private static final int CONFIG_NO_TIMEOUT_MILLIS = 20_000;
 	
 	@Before
 	public void setup() {
