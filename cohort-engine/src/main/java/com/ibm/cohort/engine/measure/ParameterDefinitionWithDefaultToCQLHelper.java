226b4280ecb3de3c4c4483be65355370af185144
matthew.sargent <matthew.sargent@ibm.com>
2021-05-04 11:20:53 -0400

    Add additional conversions

--- cohort-engine/src/main/java/com/ibm/cohort/engine/measure/ParameterDefinitionWithDefaultToCQLHelper.java
+++ cohort-engine/src/main/java/com/ibm/cohort/engine/measure/ParameterDefinitionWithDefaultToCQLHelper.java
@@ -1,15 +1,34 @@
 package com.ibm.cohort.engine.measure;
 
+import java.util.stream.Collectors;
+
 import org.hl7.fhir.r4.model.Base64BinaryType;
 import org.hl7.fhir.r4.model.BooleanType;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.DecimalType;
 import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.InstantType;
+import org.hl7.fhir.r4.model.IntegerType;
 import org.hl7.fhir.r4.model.ParameterDefinition;
+import org.hl7.fhir.r4.model.Period;
+import org.hl7.fhir.r4.model.Range;
+import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.TimeType;
 import org.hl7.fhir.r4.model.Type;
+import org.hl7.fhir.r4.model.UriType;
+import org.opencds.cqf.cql.engine.runtime.Code;
+import org.opencds.cqf.cql.engine.runtime.Concept;
+import org.opencds.cqf.cql.engine.runtime.DateTime;
+import org.opencds.cqf.cql.engine.runtime.Interval;
+import org.opencds.cqf.cql.engine.runtime.Quantity;
+import org.opencds.cqf.cql.engine.runtime.Ratio;
+import org.opencds.cqf.cql.engine.runtime.Time;
 
 import com.ibm.cohort.engine.cdm.CDMConstants;
+import com.ibm.cohort.engine.measure.parameter.UnsupportedFhirTypeException;
 import com.ibm.cohort.engine.parameter.DateParameter;
 import com.ibm.cohort.engine.parameter.DatetimeParameter;
 
@@ -31,13 +50,87 @@ public class ParameterDefinitionWithDefaultToCQLHelper {
 				 cqlValue = new DateParameter(((DateType) extensionValue).asStringValue()).toCqlType();
 			}
 			else if (extensionValue instanceof DateTimeType) {
-				cqlValue = new DatetimeParameter(((DateTimeType) extensionValue).getValueAsString()).toCqlType();
+				cqlValue = convertDateTimeType((DateTimeType) extensionValue);
 			}
 			else if (extensionValue instanceof DecimalType) {
 				cqlValue = ((DecimalType) extensionValue).getValue();
 			}
+			else if (extensionValue instanceof InstantType) {
+				cqlValue = new DatetimeParameter(((InstantType) extensionValue).getValueAsString()).toCqlType();
+			}
+			else if (extensionValue instanceof IntegerType) {
+				cqlValue = ((IntegerType) extensionValue).getValue();
+			}
+			else if (extensionValue instanceof StringType) {
+				cqlValue = ((StringType) extensionValue).getValue();
+			}
+			else if (extensionValue instanceof TimeType) {
+				cqlValue = new Time(((TimeType) extensionValue).asStringValue());
+			}
+			else if (extensionValue instanceof UriType) {
+				cqlValue = ((UriType) extensionValue).getValue();
+			}
+			else if (extensionValue instanceof Coding) {
+				cqlValue = convertCoding((Coding) extensionValue);
+			}
+			else if (extensionValue instanceof CodeableConcept) {
+				cqlValue = convertCodeableConcept((CodeableConcept) extensionValue);
+			}
+			else if (extensionValue instanceof Period) {
+				Period castValue = (Period) extensionValue;
+
+				cqlValue = new Interval(convertDateTimeType(castValue.getStartElement()), true,
+										convertDateTimeType(castValue.getEndElement()), true);
+			}
+			else if (extensionValue instanceof org.hl7.fhir.r4.model.Quantity) {
+				cqlValue = convertQuantity((org.hl7.fhir.r4.model.Quantity) extensionValue);
+			}
+			else if (extensionValue instanceof Range) {
+				Range castValue = (Range) extensionValue;
+
+				cqlValue = new Interval(convertQuantity(castValue.getLow()), true,
+										convertQuantity(castValue.getHigh()), true);
+			}
+			else if (extensionValue instanceof org.hl7.fhir.r4.model.Ratio) {
+				org.hl7.fhir.r4.model.Ratio castValue = (org.hl7.fhir.r4.model.Ratio) extensionValue;
+
+				cqlValue = new Ratio()
+						.setDenominator(convertQuantity(castValue.getDenominator()))
+						.setNumerator(convertQuantity(castValue.getNumerator()));
+			}
+			else {
+				throw new UnsupportedFhirTypeException(extensionValue);
+			}
 		}
 
 		return cqlValue;
+	}
+
+	private static Code convertCoding(Coding coding) {
+		return new Code().withCode(coding.getCode())
+				.withSystem(coding.getSystem())
+				.withDisplay(coding.getDisplay())
+				.withVersion(coding.getVersion());
+	}
+
+	private static Concept convertCodeableConcept(CodeableConcept codeableConcept) {
+		Concept concept = new Concept();
+		concept.withDisplay(codeableConcept.getText());
+
+		concept.withCodes(
+				codeableConcept.getCoding()
+						.stream()
+						.map(ParameterDefinitionWithDefaultToCQLHelper::convertCoding)
+						.collect(Collectors.toList()));
+
+		return concept;
+	}
+
+	private static DateTime convertDateTimeType(DateTimeType dateTimeType) {
+		return (DateTime) new DatetimeParameter(dateTimeType.getValueAsString()).toCqlType();
+	}
+
+	private static Quantity	convertQuantity(org.hl7.fhir.r4.model.Quantity fhirQuantity) {
+		return new Quantity().withUnit(fhirQuantity.getUnit()).withValue(fhirQuantity.getValue());
 	}
 }
--- cohort-engine/src/test/java/com/ibm/cohort/engine/measure/ParameterDefinitionWithDefaultToCQLHelperTest.java
+++ cohort-engine/src/test/java/com/ibm/cohort/engine/measure/ParameterDefinitionWithDefaultToCQLHelperTest.java
@@ -6,18 +6,38 @@ import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hl7.fhir.r4.model.Annotation;
+import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.Base64BinaryType;
 import org.hl7.fhir.r4.model.BooleanType;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.DecimalType;
+import org.hl7.fhir.r4.model.InstantType;
+import org.hl7.fhir.r4.model.IntegerType;
 import org.hl7.fhir.r4.model.ParameterDefinition;
+import org.hl7.fhir.r4.model.Period;
+import org.hl7.fhir.r4.model.Range;
+import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.TimeType;
+import org.hl7.fhir.r4.model.UriType;
 import org.junit.Test;
+import org.opencds.cqf.cql.engine.runtime.Code;
+import org.opencds.cqf.cql.engine.runtime.Concept;
 import org.opencds.cqf.cql.engine.runtime.Date;
 import org.opencds.cqf.cql.engine.runtime.DateTime;
+import org.opencds.cqf.cql.engine.runtime.Interval;
+import org.opencds.cqf.cql.engine.runtime.Quantity;
+import org.opencds.cqf.cql.engine.runtime.Ratio;
+import org.opencds.cqf.cql.engine.runtime.Time;
 
 import com.ibm.cohort.engine.cdm.CDMConstants;
+import com.ibm.cohort.engine.measure.parameter.UnsupportedFhirTypeException;
 
 public class ParameterDefinitionWithDefaultToCQLHelperTest {
 
@@ -35,12 +55,11 @@ public class ParameterDefinitionWithDefaultToCQLHelperTest {
 	@Test
 	public void testBoolean__shouldReturnBoolean() {
 		ParameterDefinition parameterDefinition = getBaseParameterDefinition("boolean");
-		Boolean expectedBoolean = true;
-		BooleanType value = new BooleanType(expectedBoolean);
+		BooleanType value = new BooleanType(true);
 
 		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
 
-		assertEquals(expectedBoolean, ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition));
+		assertEquals(true, ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition));
 	}
 
 	@Test
@@ -97,6 +116,216 @@ public class ParameterDefinitionWithDefaultToCQLHelperTest {
 		assertEquals(bigDecimalValue, ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition));
 	}
 
+	@Test
+	public void testInstant__shouldReturnDateTime() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("instant");
+
+		InstantType value = new InstantType("2020-01-01T12:30:00.0Z");
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		assertTrue(new DateTime("2020-01-01T12:30:00.0", ZoneOffset.UTC).equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testInteger__shouldReturnInteger() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("integer");
+
+		int expectedValue = 10;
+		IntegerType value = new IntegerType(expectedValue);
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		assertEquals(expectedValue, ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition));
+	}
+
+	@Test
+	public void testString__shouldReturnString() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("string");
+
+		String expectedValue = "data";
+		StringType value = new StringType(expectedValue);
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		assertEquals(expectedValue, ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition));
+	}
+
+	@Test
+	public void testTime__shouldReturnTime() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("time");
+
+		String timeString = "12:30:00";
+		TimeType value = new TimeType(timeString);
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Time expectedValue = new Time(timeString);
+
+		assertTrue(expectedValue.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testUri__shouldReturnString() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("uri");
+
+		String uriString = "a-b-c-d-e-f-g";
+		UriType value = new UriType(uriString);
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		assertEquals(uriString, ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition));
+	}
+
+	@Test(expected = UnsupportedFhirTypeException.class)
+	public void testAnnotation__shouldThrowException() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Annotation");
+
+		Annotation value = new Annotation();
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition);
+	}
+
+	@Test(expected = UnsupportedFhirTypeException.class)
+	public void testAttachment__shouldThrowException() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Attachment");
+
+		Attachment value = new Attachment();
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition);
+	}
+
+	@Test
+	public void testCoding__shouldReturnCode() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Coding");
+
+		Coding value = makeCoding("sys", "val", "dis", "ver");
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Code expectedCode = new Code().withSystem("sys").withCode("val").withDisplay("dis").withVersion("ver");
+
+		assertTrue(expectedCode.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testEmptyCoding__shouldReturnEmptyCode() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Coding");
+
+		Coding value = new Coding();
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Code expectedCode = new Code();
+
+		assertTrue(expectedCode.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testCodeableConcept__shouldReturnConcept() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("CodeableConcept");
+
+		CodeableConcept value = new CodeableConcept();
+		value.setText("plainText");
+
+		value.addCoding(makeCoding("s1", "val1", "d1", "ver1"));
+		value.addCoding(makeCoding("s2", "val2", "d2", "ver2"));
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		List<Code> expectedCodes = new ArrayList<>();
+		expectedCodes.add(new Code().withSystem("s1").withCode("val1").withDisplay("d1").withVersion("ver1"));
+		expectedCodes.add(new Code().withSystem("s2").withCode("val2").withDisplay("d2").withVersion("ver2"));
+
+		Concept expectedConcept = new Concept().withDisplay("plainText")
+				.withCodes(expectedCodes);
+
+		assertTrue(expectedConcept.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testPeriod__shouldReturnIntervalOfDateTime() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Period");
+
+		Period value = new Period();
+		value.setStartElement(new DateTimeType("2020-01-01T12:00:00.0"));
+		value.setEndElement(new DateTimeType("2020-02-04T11:00:00.0-05:00"));
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Interval expectedInterval = new Interval(new DateTime("2020-01-01T12:00:00.0", ZoneOffset.UTC), true,
+												 new DateTime("2020-02-04T11:00:00.0-05:00", ZoneOffset.UTC), true);
+
+		assertTrue(expectedInterval.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testQuantity__shouldReturnQuantity() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Quantity");
+
+		BigDecimal bigDecimalValue = new BigDecimal(1.5);
+		String unit = "ml";
+
+		org.hl7.fhir.r4.model.Quantity value = new org.hl7.fhir.r4.model.Quantity();
+		value.setUnit(unit);
+		value.setValue(bigDecimalValue);
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Quantity expectedQuantity = new Quantity()
+				.withUnit(unit)
+				.withValue(bigDecimalValue);
+
+		assertTrue(expectedQuantity.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testRange__shouldReturnIntervalOfQuantity() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Range");
+
+		BigDecimal lowValue = new BigDecimal(1.5);
+		BigDecimal highValue = new BigDecimal(2.5);
+		String unit = "ml";
+
+		Range value = new Range()
+				.setLow(new org.hl7.fhir.r4.model.Quantity().setValue(lowValue).setUnit(unit))
+				.setHigh(new org.hl7.fhir.r4.model.Quantity().setValue(highValue).setUnit(unit));
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Interval expectedInterval = new Interval(new Quantity().withUnit(unit).withValue(lowValue),
+												 true,
+												 new Quantity().withUnit(unit).withValue(highValue),
+												 true);
+
+		assertTrue(expectedInterval.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
+	@Test
+	public void testRatio__shouldReturnRatio() {
+		ParameterDefinition parameterDefinition = getBaseParameterDefinition("Ratio");
+
+		BigDecimal denominatorValue = new BigDecimal(1.5);
+		BigDecimal numeratorValue = new BigDecimal(2.5);
+		String unit = "ml";
+
+		org.hl7.fhir.r4.model.Ratio value = new org.hl7.fhir.r4.model.Ratio()
+				.setDenominator(new org.hl7.fhir.r4.model.Quantity().setValue(denominatorValue).setUnit(unit))
+				.setNumerator(new org.hl7.fhir.r4.model.Quantity().setValue(numeratorValue).setUnit(unit));
+
+		parameterDefinition.addExtension(CDMConstants.PARAMETER_DEFAULT_URL, value);
+
+		Ratio expectedInterval = new Ratio()
+				.setDenominator(new Quantity().withUnit(unit).withValue(denominatorValue))
+				.setNumerator(new Quantity().withUnit(unit).withValue(numeratorValue));
+
+		assertTrue(expectedInterval.equal(ParameterDefinitionWithDefaultToCQLHelper.getCqlObject(parameterDefinition)));
+	}
+
 	private ParameterDefinition getBaseParameterDefinition(String type) {
 		ParameterDefinition parameterDefinition = new ParameterDefinition();
 		parameterDefinition.setName(type + "Param");
@@ -104,5 +333,16 @@ public class ParameterDefinitionWithDefaultToCQLHelperTest {
 		parameterDefinition.setType(type);
 
 		return parameterDefinition;
+	}
+
+	private Coding makeCoding(String system, String value, String display, String version) {
+		Coding coding = new Coding();
+
+		coding.setSystem(system);
+		coding.setCode(value);
+		coding.setDisplay(display);
+		coding.setVersion(version);
+
+		return coding;
 	}
 }
