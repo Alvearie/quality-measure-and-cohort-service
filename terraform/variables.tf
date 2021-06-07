cad351dbc297ce55b6c30ffa1973b10a3572cce3
TimBishop <tabishop@us.ibm.com>
2021-06-07 14:24:00 -0500

    vpc updates

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+# *****************************************************************
+#
+# Licensed Materials - Property of IBM
+#
+# (C) Copyright IBM Corp. 2021. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# *****************************************************************
+variable "namespace" {
+  description = "Kubernetes namespace to deploy to."
+  type        = string
+}
+
+variable "service_names" {
+  description = "List of services names (DNS names) include as ALT names in generated TLS certificate."
+  type        = list(string)
+}
+
+variable "organization" {
+  description = "Organization name (OU) for TLS certificate"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "Resource group"
+  type = string
+}
+
+variable "kubernetes_config_context" {
+  description = "k8s cluster config context used by the k8s terraform provider"
+  type = string
+}
