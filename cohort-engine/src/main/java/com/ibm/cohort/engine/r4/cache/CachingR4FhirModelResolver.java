98ff75f38a56fc65cc9c25c5dfb30266779c6e7d
matthew.sargent <matthew.sargent@ibm.com>
2021-07-06 16:53:38 -0400

    Cache name

--- cohort-engine/src/main/java/com/ibm/cohort/engine/r4/cache/CachingR4FhirModelResolver.java
+++ cohort-engine/src/main/java/com/ibm/cohort/engine/r4/cache/CachingR4FhirModelResolver.java
@@ -1,7 +1,5 @@
 package com.ibm.cohort.engine.r4.cache;
 
-import java.util.UUID;
-
 import javax.cache.Cache;
 import javax.cache.Caching;
 import javax.cache.configuration.CompleteConfiguration;
@@ -11,20 +9,14 @@ import org.opencds.cqf.cql.engine.fhir.model.R4FhirModelResolver;
 
 public class CachingR4FhirModelResolver extends R4FhirModelResolver {
 
-	private static final String CACHE_ID_PREFIX = "default-r4-fhir-model-type-cache";
-
-	// Create a unique cacheId to prevent conflicts with other caches in the same JVM.
-	private static String createCacheId() {
-		String uuid = UUID.randomUUID().toString();
-		return CACHE_ID_PREFIX + uuid;
-	}
+	private static final String CACHE_ID = "default-r4-fhir-model-type-cache";
 
 	private static CompleteConfiguration<String, Class<?>> getDefaultConfiguration() {
 		return new MutableConfiguration<String, Class<?>>()
 				.setStoreByValue(false);
 	}
 
-	private static final Cache<String, Class<?>> CACHE = Caching.getCachingProvider().getCacheManager().createCache(createCacheId(), getDefaultConfiguration());
+	private static final Cache<String, Class<?>> CACHE = Caching.getCachingProvider().getCacheManager().createCache(CACHE_ID, getDefaultConfiguration());
 
 	public CachingR4FhirModelResolver() {
 	}
