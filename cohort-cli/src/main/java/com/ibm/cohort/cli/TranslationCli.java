13642e302d6d72c56ebaf08e150b61bfc9689733
shelley.murphy <mshelley@us.ibm.com>
2021-07-30 16:34:35 -0400

    Adding test for translationCli
    Test rename

--- cohort-cli/src/main/java/com/ibm/cohort/cli/TranslationCli.java
+++ cohort-cli/src/main/java/com/ibm/cohort/cli/TranslationCli.java
@@ -11,74 +11,50 @@ package com.ibm.cohort.cli;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintStream;
-import java.util.zip.ZipInputStream;
 
 import org.cqframework.cql.elm.execution.Library;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.internal.DefaultConsole;
-import com.ibm.cohort.engine.ZipStreamLibrarySourceProvider;
+import com.ibm.cohort.engine.DirectoryLibrarySourceProvider;
+import com.ibm.cohort.translator.provider.CqlTranslationProvider;
 import com.ibm.cohort.translator.provider.InJVMCqlTranslationProvider;
 
 public class TranslationCli {
 
-//	public static final LibraryFormat DEFAULT_SOURCE_FORMAT = LibraryFormat.XML;
-
-	//todo trim arguments
 	private static final class TranslationOptions extends ConnectionArguments {
 		@Parameter(names = { "-f",
 				"--files" }, description = "Path to cql file", required = true)
 		private String cqlPath;
 
-//		@Parameter(names = { "-s" }, description = "Additional filters to apply to library loaders if supported by the library loading mechansim")
-//		private List<String> filters;
-//
-//		@Parameter(names = { "-l",
-//				"--libraryName" }, description = "Library Name", required = true)
-//		private String libraryName;
-//
-//		@Parameter(names = { "-v",
-//				"--libraryVersion" }, description = "Library Version", required = false)
-//		private String libraryVersion;
-//
-//		@Parameter(names = { "-e", "--expression" }, description = "ELM Expression(s) to Execute", required = false)
-//		private Set<String> expressions;
-//
-//		@Parameter(names = { "-c",
-//				"--context-id" }, description = "Unique ID for one or more context objects (e.g. Patient IDs)", required = true)
-//		private List<String> contextIds;
-//
-//		@Parameter(names = { "-p",
-//				"--parameters" }, description = "Parameter value(s) in format name:type:value where value can contain additional parameterized elements separated by comma. Multiple parameters must be specified as multiple -p options", splitter = NoSplittingSplitter.class, required = false)
-//		private List<String> parameters;
-
-//		@Parameter(names = { "-s",
-//				"--source-format" }, description = "Indicates which files in the file source should be processed", required = false)
-//		private LibraryFormat sourceFormat = DEFAULT_SOURCE_FORMAT;
+		@Parameter(names = { "-b", "--dependency-directory" }, description = "Directory containing additional files necessary for primary file's translation")
+		private File directory;
 
 		@Parameter(names = { "-i",
 				"--model-info" }, description = "Model info file used when translating CQL")
 		private File modelInfoFile;
 
-//		@Parameter(names = {"--logging-level" }, description = "Specific logging level")
-//		private LoggingEnum loggingLevel = LoggingEnum.NA;
-
-//		@Parameter(names = { "-h", "--help" }, description = "Display this help", required = false, help = true)
-//		private boolean isDisplayHelp;
+		@Parameter(names = { "-h", "--help" }, description = "Display this help", required = false, help = true)
+		private boolean isDisplayHelp;
 
 	}
 
 	public void runWithArgs(TranslationOptions options, PrintStream out) throws Exception {
+		CqlTranslationProvider provider;
+		if(options.directory != null && options.directory.exists()){
+			DirectoryLibrarySourceProvider librarySourceProvider = new DirectoryLibrarySourceProvider(options.directory.toPath());
+			provider = new InJVMCqlTranslationProvider(librarySourceProvider);
+		}
+		else{
+			provider = new InJVMCqlTranslationProvider();
+		}
 
-		//todo assuming that we want to provide a zip file, need a given structure and resolved search paths
-		ZipStreamLibrarySourceProvider sourceProvider = new ZipStreamLibrarySourceProvider(new ZipInputStream(new FileInputStream(options.modelInfoFile)), "cql");
-		//todo allow non-zip input
+		if (options.modelInfoFile != null && options.modelInfoFile.exists()) {
+			provider.convertAndRegisterModelInfo(options.modelInfoFile);
+		}
 
-		InJVMCqlTranslationProvider provider = new InJVMCqlTranslationProvider();
-		provider.addLibrarySourceProvider(sourceProvider);
-
-		Library library = provider.translate(options.cqlPath);
+		Library library = provider.translate(new FileInputStream(new File(options.cqlPath)));
 
 		out.println("Translated Library: ");
 		out.println(library.toString());
@@ -90,6 +66,11 @@ public class TranslationCli {
 				.console(new DefaultConsole(System.out))
 				.addObject(options).build();
 		jc.parse(args);
+
+		if (options.isDisplayHelp) {
+			jc.usage();
+			System.exit(0);
+		}
 
 		TranslationCli wrapper = new TranslationCli();
 		wrapper.runWithArgs(options, System.out);
