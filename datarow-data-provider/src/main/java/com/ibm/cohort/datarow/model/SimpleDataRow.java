9cec7545686b036aeb6da07e83aea7db2c76fc01
Corey Sanders <corey.thecolonel@gmail.com>
2021-08-05 13:22:41 -0400

    Normalize tabs and spaces
    Signed-off-by: Corey Sanders <corey.thecolonel@gmail.com>
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

--- datarow-data-provider/src/main/java/com/ibm/cohort/datarow/model/SimpleDataRow.java
+++ datarow-data-provider/src/main/java/com/ibm/cohort/datarow/model/SimpleDataRow.java
@@ -12,14 +12,14 @@ import java.util.Objects;
 import java.util.Set;
 
 /**
- * Map-based implementation of DataRow. Contents of the map are expected
- * to be in whatever final form is required for the CQL Engine. For example,
- * date and datetime values should be in the CQL Runtime typesystem. 
+ * Map-based implementation of DataRow. Contents of the map are expected to be
+ * in whatever final form is required for the CQL Engine. For example, date and
+ * datetime values should be in the CQL Runtime typesystem.
  */
 public class SimpleDataRow implements Serializable, DataRow {
-	private static final long serialVersionUID = 1L;
-	
-	private final Map<String, Object> fields;
+    private static final long serialVersionUID = 1L;
+
+    private final Map<String, Object> fields;
 
     public SimpleDataRow(Map<String, Object> fields) {
         this.fields = fields;
@@ -37,30 +37,28 @@ public class SimpleDataRow implements Serializable, DataRow {
 
     @Override
     public String toString() {
-        return "DataRow{" +
-                "fields=" + fields +
-                '}';
+        return "DataRow{" + "fields=" + fields + '}';
     }
-    
-    @Override 
+
+    @Override
     public boolean equals(Object rhs) {
-    	boolean isEqual = false;
-    	if( rhs != null && rhs instanceof DataRow ) {
-    		DataRow row = (DataRow) rhs;
-    		if( getFieldNames().equals(row.getFieldNames()) ) {
-	    		for( String fieldName : getFieldNames() ) { 
-		    		isEqual = Objects.equals(getValue(fieldName), row.getValue(fieldName));
-		    		if( ! isEqual ) { 
-		    			break;
-		    		}
-		    	}
-    		}
-    	}
-    	return isEqual;
+        boolean isEqual = false;
+        if (rhs != null && rhs instanceof DataRow) {
+            DataRow row = (DataRow) rhs;
+            if (getFieldNames().equals(row.getFieldNames())) {
+                for (String fieldName : getFieldNames()) {
+                    isEqual = Objects.equals(getValue(fieldName), row.getValue(fieldName));
+                    if (!isEqual) {
+                        break;
+                    }
+                }
+            }
+        }
+        return isEqual;
     }
-    
+
     @Override
     public int hashCode() {
-    	return fields.hashCode();
+        return fields.hashCode();
     }
 }
