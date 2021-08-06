111a14da91e391e66c97e92d65646c27026f65f1
Corey Sanders <corey.thecolonel@gmail.com>
2021-08-06 16:01:49 -0400

    Add support for multiple model info, multiple pages of keys in object listings
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

--- cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/aws/AWSClientHelpers.java
+++ cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/aws/AWSClientHelpers.java
@@ -6,6 +6,13 @@
 
 package com.ibm.cohort.cql.aws;
 
+import java.util.function.Consumer;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 public class AWSClientHelpers {
     public static String toS3Url(String bucket, String inputPath, String filename) {
         return String.format(
@@ -14,5 +21,27 @@ public class AWSClientHelpers {
                 inputPath,
                 filename
         );
+    }
+    
+    public static void processS3Objects(AmazonS3 s3client, String bucket, String keyPrefix, Consumer<String> consumer) {
+        processS3ObjectKeys(s3client, bucket, keyPrefix, osm -> {
+            String obj = s3client.getObjectAsString(bucket, osm.getKey());
+            consumer.accept(obj);
+        });
+    }
+    
+    public static void processS3ObjectKeys(AmazonS3 s3client, String bucket, String keyPrefix, Consumer<S3ObjectSummary> consumer) {
+        //https://docs.aws.amazon.com/AmazonS3/latest/userguide/ListingKeysUsingAPIs.html
+        ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucket).withPrefix(keyPrefix);
+        ListObjectsV2Result result;
+        do {
+            result = s3client.listObjectsV2(req);
+            result.getObjectSummaries().forEach( osm -> {
+                consumer.accept(osm);
+            });
+            
+            String token = result.getContinuationToken();
+            req.setContinuationToken(token);
+        } while( result.isTruncated() );
     }
 }
