6a68ebd2a22d5695306b172a8ab809890541f7e1
Corey Sanders <corey.thecolonel@gmail.com>
2021-08-06 10:46:48 -0400

    Split apis from spark impl, rename projects, add tests
    I split the API-type stuff out away from the Spark project and then did
    a project rename on the spark and datarow provider projects because the
    previous names were bother me. I also implemented a bunch of additional
    tests.

---
+++ cohort-evaluator-spark/src/test/java/com/ibm/cohort/cql/aws/AWSClientConfigFactoryTest.java
@@ -0,0 +1,31 @@
+package com.ibm.cohort.cql.aws;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class AWSClientConfigFactoryTest {
+    @Test
+    public void testConfigFromEnvironment() {
+
+        String expectedAccess = "access";
+        String expectedSecret = "secret";
+        String expectedEndpoint = "endpoint";
+        String expectedLocation = "location";
+
+        Map<String, String> env = new HashMap<>();
+        env.put(EnvConstants.AWS_ACCESS_KEY_KEY, expectedAccess);
+        env.put(EnvConstants.AWS_SECRET_KEY_KEY, expectedSecret);
+        env.put(EnvConstants.AWS_ENDPOINT_KEY, expectedEndpoint);
+        env.put(EnvConstants.AWS_LOCATION_KEY, expectedLocation);
+
+        AWSClientConfig config = AWSClientConfigFactory.fromMap(env);
+        assertEquals(expectedAccess, config.getAwsAccessKey());
+        assertEquals(expectedSecret, config.getAwsSecretKey());
+        assertEquals(expectedEndpoint, config.getAwsEndpoint());
+        assertEquals(expectedLocation, config.getAwsLocation());
+    }
+}
---
+++ cohort-evaluator/src/test/java/com/ibm/cohort/cql/library/CqlLibraryDeserializationExceptionTest.java
@@ -0,0 +1,28 @@
+package com.ibm.cohort.cql.library;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CqlLibraryDeserializationExceptionTest {
+    @Test
+    public void testInitializeSuccess() {
+        String expectedMessage = "My message";
+        Exception expectedRootCause = new IllegalArgumentException("That's wrong!");
+
+        
+        CqlLibraryDeserializationException ex;
+        
+        ex = new CqlLibraryDeserializationException();
+        
+        ex = new CqlLibraryDeserializationException(expectedMessage);
+        assertEquals(expectedMessage, ex.getMessage());
+        
+        ex = new CqlLibraryDeserializationException(expectedRootCause);
+        assertEquals(ex.getCause(), expectedRootCause);
+        
+        ex = new CqlLibraryDeserializationException(expectedMessage, expectedRootCause);
+        assertEquals(expectedMessage, ex.getMessage());
+        assertEquals(ex.getCause(), expectedRootCause);
+    }
+}
