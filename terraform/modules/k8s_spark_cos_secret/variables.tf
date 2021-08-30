70b6580818ba8f6ea4cb4fe987ee04a6665261d9
Corey Sanders <corey.thecolonel@gmail.com>
2021-08-30 16:24:50 -0400

    Add in provisioning for COS-backed PVC config volume
    Signed-off-by: Corey Sanders <corey.thecolonel@gmail.com>
    Signed-off-by: TimBishop <tabishop@us.ibm.com>

--- terraform/modules/k8s_spark_cos_secret/variables.tf
+++ terraform/modules/k8s_spark_cos_secret/variables.tf
@@ -4,26 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 variable "spark_cos_secret_name" {
-  description = "Name of Cloud Object Store (cos) secret used by spark to access cos"
+  description = "Name of Cloud Object Store (COS) secret used by spark to access COS"
   type = string
 }
 variable "spark_cos_secret_namespace" {
-  description = "Namespace containing cos secret used by spark to access cos"
+  description = "Namespace containing COS secret used by spark to access COS"
   type = string
 }
-variable "spark_cos_aws_secret_key" {
-  description = "AWS secret key value for cos"
+variable "spark_cos_resource_key_manager" {
+  description = "IBM Resource Key name for Manager role in COS configuration bucket"
   type = string
 }
-variable "spark_cos_aws_access_key" {
-  description = "AWS access key value for cos"
+variable "spark_cos_resource_key_writer" {
+  description = "IBM Resource Key name for Writer role in COS configuration bucket"
   type = string
 }
-variable "spark_cos_aws_endpoint" {
-  description = "AWS endpoint value for cos"
+variable "config_volume_claim_name" {
+  description = "Kubernetes PVC resource name for configuration data"
   type = string
 }
-variable "spark_cos_aws_location" {
-  description = "AWS location value for cos"
+variable "config_bucket_name" {
+  description = "COS bucket for configuration storing configuration data"
   type = string
 }
