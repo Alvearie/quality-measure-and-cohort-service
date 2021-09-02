7f7eec3b2ca1515132f347b2f8f93bdb614a3294
David Kwasny <dkwasny@us.ibm.com>
2021-09-02 17:59:34 -0400

    Cleaning up the join logic
    Integrating ContextRetriever into SparkCqlEvaluator.
    Creating the DatasetRetriever classes to assisst unit testing down the road.
    
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

---
+++ cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/CsvDatasetRetriever.java
@@ -0,0 +1,31 @@
+/*
+ * (C) Copyright IBM Corp. 2021
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.cohort.cql.spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+// TODO: Remove once unit tests are setup
+public class CsvDatasetRetriever implements DatasetRetriever {
+
+    private final SparkSession spark;
+
+    public CsvDatasetRetriever(SparkSession spark) {
+        this.spark = spark;
+    }
+
+    @Override
+    public Dataset<Row> readDataset(String path) {
+        return spark.read()
+                // TODO: Parquet
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv(path);
+    }
+
+}
