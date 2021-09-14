b54adb8f4e79fbeddacae6a51fadf41da9b896b3
David Kwasny <dkwasny@us.ibm.com>
2021-09-14 19:35:33 -0400

    Adding Javadocs and comments where sensible
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

--- cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/aggregation/ContextRetriever.java
+++ cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/aggregation/ContextRetriever.java
@@ -21,7 +21,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-// TODO: Javadoc all the things
+/**
+ * Handles the retrieval and organization of context data.
+ *
+ * Given a {@link DatasetRetriever} and mappings from datatype to path,
+ * this class and process any number of contexts.
+ */
 public class ContextRetriever {
 
     public static final String SOURCE_FACT_IDX = "__SOURCE_FACT";
@@ -30,14 +35,23 @@ public class ContextRetriever {
     private final Map<String, String> inputPaths;
     private final DatasetRetriever datasetRetriever;
 
+    /**
+     * @param inputPaths A mapping from datatype to Hadoop compatible path
+     * @param datasetRetriever A {@link DatasetRetriever} for low level data retrieval
+     */
     public ContextRetriever(Map<String, String> inputPaths, DatasetRetriever datasetRetriever) {
         this.inputPaths = inputPaths;
         this.datasetRetriever = datasetRetriever;
     }
 
+    /**
+     * Retrieves, joins, and organizes all data for a {@link ContextDefinition} into
+     * a single {@link JavaPairRDD}.
+     *
+     * @param contextDefinition The {@link ContextDefinition} to process
+     * @return A {@link JavaPairRDD} linking contextValue to a {@link List} of {@link Row}s
+     */
     public JavaPairRDD<Object, List<Row>> retrieveContext(ContextDefinition contextDefinition) {
-        // TODO: What about grouping first and then joining the rdds together?
-        // That might cause less serialization.
         List<JavaPairRDD<Object, Row>> rddList = gatherRDDs(contextDefinition);
 
         JavaPairRDD<Object, Row> allData = unionPairRDDs(rddList);
@@ -59,6 +73,14 @@ public class ContextRetriever {
         return retVal;
     }
 
+    /**
+     * Creates a {@link JavaPairRDD} for a context's primary datatype and all
+     * underlying joins.
+     *
+     * @param contextDefinition The {@link ContextDefinition} to process
+     * @return A {@link List} of {@link JavaPairRDD}s mapping context value to {@link Row}.
+     *         It is expected for a context value to have multiple mappings within an RDD.
+     */
     private List<JavaPairRDD<Object, Row>> gatherRDDs(ContextDefinition contextDefinition) {
         List<JavaPairRDD<Object, Row>> retVal = new ArrayList<>();
 
@@ -69,7 +91,7 @@ public class ContextRetriever {
 
         // We need to retain the original context value from the primary datatype.
         // This is done by adding a "placeholder column" that selects the context value.
-        // This is needed when we retain only the columns from the related dataset.
+        // This is needed for when we retain only the columns from the related dataset.
         Column joinContextColumn = primaryDataset.col(primaryKeyColumn).as(JOIN_CONTEXT_VALUE_IDX);
 
         List<Join> joins = contextDefinition.getRelationships() == null
@@ -92,9 +114,6 @@ public class ContextRetriever {
                 ManyToMany manyToMany = (ManyToMany) join;
                 String assocDataType = manyToMany.getAssociationDataType();
                 Dataset<Row> assocDataset = readDataset(assocDataType);
-                // TODO: Address naming.
-                // I think something simple like "left" and "right" would be easier
-                // to understand.
                 String assocPrimaryColumnName = manyToMany.getAssociationOneKeyColumn();
                 String assocRelatedColumnName = manyToMany.getAssociationManyKeyColumn();
 
@@ -126,9 +145,9 @@ public class ContextRetriever {
      * Given a set of rows that are indexed by context value, reorganize the data so
      * that all rows related to the same context are grouped into a single pair.
      *
-     * @param allData           rows mapped from context value to a single data row
-     * @return rows grouped mapped from context value to a list of all data for that
-     *         context
+     * @param allData Rows mapped from context value to a single data row.
+     * @return A single {@link JavaPairRDD} mapping context value to
+     *         the list of all data for that context.
      */
     private JavaPairRDD<Object, List<Row>> groupPairRDDs(JavaPairRDD<Object, Row> allData) {
         // Regroup data by context ID so that all input data for the same
@@ -153,6 +172,14 @@ public class ContextRetriever {
         );
     }
 
+    /**
+     * Converts a {@link Dataset} to {@link JavaPairRDD} that maps the value from
+     * the provided column to the respective {@link Row}.
+     *
+     * @param dataset The {@link Dataset} to convert.
+     * @param contextColumn The column to use as the source for the context value.
+     * @return A {@link JavaPairRDD} from context value to {@link Row}.
+     */
     private JavaPairRDD<Object, Row> toPairRDD(Dataset<Row> dataset, String contextColumn) {
         return dataset.javaRDD().mapToPair(row -> {
             Object joinValue = row.getAs(contextColumn);
@@ -160,9 +187,17 @@ public class ContextRetriever {
         });
     }
 
-    private JavaPairRDD<Object, Row> unionPairRDDs(List<JavaPairRDD<Object, Row>> rddList) {
-        JavaPairRDD<Object, Row> retVal = null;
-        for (JavaPairRDD<Object, Row> rdd : rddList) {
+    /**
+     * Unions multiple {@link JavaPairRDD}s into one larger {@link JavaPairRDD}.
+     *
+     * @param rddList The list of RDDs to union.
+     * @param <K> The key of the RDD.
+     * @param <V> The value of the RDD.
+     * @return A fully unioned RDD.
+     */
+    private <K, V> JavaPairRDD<K, V> unionPairRDDs(List<JavaPairRDD<K, V>> rddList) {
+        JavaPairRDD<K, V> retVal = null;
+        for (JavaPairRDD<K, V> rdd : rddList) {
             if (retVal == null) {
                 retVal = rdd;
             }
@@ -173,6 +208,13 @@ public class ContextRetriever {
         return retVal;
     }
 
+    /**
+     * Reads the data for the specified datatype into a {@link Dataset}.
+     * Adds the special {@value SOURCE_FACT_IDX} to keep track of what datatype
+     * a particular {@link Row} came from.
+     * @param dataType The datatype to read.
+     * @return A {@link Dataset} containing the data for the specified datatype.
+     */
     private Dataset<Row> readDataset(String dataType) {
         String path = inputPaths.get(dataType);
         if (path == null) {
