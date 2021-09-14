456adc1dc0e86ecd3a4821d6800d30e005b398b8
David Kwasny <dkwasny@us.ibm.com>
2021-09-14 19:35:33 -0400

    Adding Javadocs and comments where sensible
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

--- cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/data/DefaultDatasetRetriever.java
+++ cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/data/DefaultDatasetRetriever.java
@@ -11,15 +11,25 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+/**
+ * A {@link DatasetRetriever} that reads data from a provided {@link SparkSession}.
+ */
 public class DefaultDatasetRetriever implements DatasetRetriever {
 
     private final SparkSession spark;
     private final String inputFormat;
 
+    /**
+     * @param spark The {@link SparkSession} to read from.
+     */
     public DefaultDatasetRetriever(SparkSession spark) {
         this(spark, null);
     }
 
+    /**
+     * @param spark The {@link SparkSession} to read from.
+     * @param inputFormat The Spark input format to use for all read operations.
+     */
     public DefaultDatasetRetriever(SparkSession spark, String inputFormat) {
         this.spark = spark;
         this.inputFormat = inputFormat;
