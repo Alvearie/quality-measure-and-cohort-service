bcb53acad7f082c556a66a74d119ba964f66f33d
matthew.sargent <matthew.sargent@ibm.com>
2021-09-21 14:24:06 -0400

    import
    Signed-off-by: matthew.sargent <matthew.sargent@ibm.com>

--- cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/data/SparkSchemaCreator.java
+++ cohort-evaluator-spark/src/main/java/com/ibm/cohort/cql/spark/data/SparkSchemaCreator.java
@@ -14,7 +14,6 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.cqframework.cql.cql2elm.ModelInfoLoader;
-import org.cqframework.cql.elm.execution.ExpressionDef;
 import org.cqframework.cql.elm.execution.Library;
 import org.hl7.elm.r1.VersionedIdentifier;
 import org.hl7.elm_modelinfo.r1.ClassInfo;
