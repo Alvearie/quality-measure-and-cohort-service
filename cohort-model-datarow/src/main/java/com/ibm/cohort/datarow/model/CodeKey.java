fc4307d59b1180c53d6f6e13488da5c5763b2777
Corey Sanders <corey.thecolonel@gmail.com>
2021-09-21 17:49:50 -0400

    Beef up the documentation in CodeKey
    Signed-off-by: Corey Sanders <corey.thecolonel@gmail.com>

--- cohort-model-datarow/src/main/java/com/ibm/cohort/datarow/model/CodeKey.java
+++ cohort-model-datarow/src/main/java/com/ibm/cohort/datarow/model/CodeKey.java
@@ -11,7 +11,9 @@ import org.opencds.cqf.cql.engine.runtime.Code;
 /**
  * This class is used as the map key for a cache in the retrieve provider that indexes
  * data rows based on the codings contained in that data. Code equivalence checks
- * are used for lookup per the CQL specification on filtered retrieves.
+ * are used for lookup per the CQL specification on filtered retrieves. Code equivalence
+ * checking only considers the system and code values of the Code object. The version
+ * and display properties are ignored.
  * 
  * @see <a href="https://cql.hl7.org/02-authorsguide.html#filtering-with-terminology">Filtering with Terminology</a>
  * @see <a href="https://cql.hl7.org/02-authorsguide.html#terminology-operators">Terminology Operations</a>
@@ -27,12 +29,23 @@ public class CodeKey extends Code {
                 .withVersion(otherCode.getVersion());
     }
 
+    /**
+     * Checks for equality between one CodeKey and another. Per the CQL Spec, the
+     * "equivalence" operator is used when doing filtered retrieves. Equivalence
+     * considers only the system and code properties of the <code>Code</code>
+     * object. The display and version properties of the code are ignored.
+     */
     @Override
     public boolean equals(Object o2) {
         Boolean codeEquals = super.equivalent(o2);
         return codeEquals != null && codeEquals;
     }
 
+    /**
+     * Computes object identity based on the same logic as Code equivalence. Only
+     * the system and code properties of the <code>Code</code> object are used. The
+     * display and version properties of the code are ignored.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
