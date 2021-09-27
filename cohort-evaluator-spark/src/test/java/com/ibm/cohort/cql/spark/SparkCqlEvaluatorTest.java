dcc99ceae583fa284ad7beb89ff91a9f357a130c
matthew.sargent <matthew.sargent@ibm.com>
2021-09-27 17:46:46 -0400

    remove try block after conversation with Kwas
    Signed-off-by: matthew.sargent <matthew.sargent@ibm.com>

--- cohort-evaluator-spark/src/test/java/com/ibm/cohort/cql/spark/SparkCqlEvaluatorTest.java
+++ cohort-evaluator-spark/src/test/java/com/ibm/cohort/cql/spark/SparkCqlEvaluatorTest.java
@@ -145,13 +145,13 @@ public class SparkCqlEvaluatorTest extends BaseSparkTest {
 
     private void validateOutputCountsAndColumns(String filename, Set<String> columnNames, int numExpectedRows) {
         // SparkCqlEvaluator closes the SparkSession. Make sure we have one opened before any validation. 
-        try(SparkSession sparkSession = initializeSession(Java8API.ENABLED)) {
-            Dataset<Row> results = sparkSession.read().parquet(filename);
-            validateColumnNames(results.schema(), columnNames);
+        SparkSession sparkSession = initializeSession(Java8API.ENABLED);
+        Dataset<Row> results = sparkSession.read().parquet(filename);
+        validateColumnNames(results.schema(), columnNames);
 
-            assertEquals(numExpectedRows, results.count());
-        }
+        assertEquals(numExpectedRows, results.count());
     }
+    
 
     private void validateColumnNames(StructType schema, Set<String> columnNames) {
         for (String field : schema.fieldNames()) {
@@ -241,20 +241,20 @@ public class SparkCqlEvaluatorTest extends BaseSparkTest {
     
     private void validateOutput(String filename, List<Row> expectedRows, StructType schema) {
         // SparkCqlEvaluator closes the SparkSession. Make sure we have one opened before any validation.
-        try(SparkSession sparkSession = initializeSession(Java8API.ENABLED)){
-            Dataset<Row> actualDataFrame = sparkSession.read().parquet(filename);
-            // Column names with a dot in them need escaped with backticks
-            List<String> columnList = Arrays.asList(actualDataFrame.columns());
-            String[] actualColumns = columnList.toArray(new String[columnList.size()]);
+        SparkSession sparkSession = initializeSession(Java8API.ENABLED);
+        Dataset<Row> actualDataFrame = sparkSession.read().parquet(filename);
+        // Column names with a dot in them need escaped with backticks
+        List<String> columnList = Arrays.asList(actualDataFrame.columns());
+        String[] actualColumns = columnList.toArray(new String[columnList.size()]);
 
-            // Make sure columns are in the same order in both dataframes
-            Dataset<Row> expectedDataFrame = sparkSession.createDataFrame(expectedRows, schema)
-                    .select(actualColumns[0], Arrays.copyOfRange(actualColumns, 1, actualColumns.length));
+        // Make sure columns are in the same order in both dataframes
+        Dataset<Row> expectedDataFrame = sparkSession.createDataFrame(expectedRows, schema)
+                .select(actualColumns[0], Arrays.copyOfRange(actualColumns, 1, actualColumns.length));
 
-            assertEquals(schema.fields().length, actualColumns.length);
-            assertEquals(0, expectedDataFrame.except(actualDataFrame).count());
-            assertEquals(0, actualDataFrame.except(expectedDataFrame).count());
-        }
+        assertEquals(schema.fields().length, actualColumns.length);
+        assertEquals(0, expectedDataFrame.except(actualDataFrame).count());
+        assertEquals(0, actualDataFrame.except(expectedDataFrame).count());
+
     }
 
     @Test(expected = IllegalArgumentException.class)
