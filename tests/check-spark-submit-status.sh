bf9e6a3fad2a99d45e54fa0104edc373bc483081
Jai D Goradia <goradia@us.ibm.com>
2021-10-06 12:01:25 -0500

    Address PR feedback
    Signed-off-by: Jai D Goradia <goradia@us.ibm.com>

--- tests/check-spark-submit-status.sh
+++ tests/check-spark-submit-status.sh
@@ -8,19 +8,19 @@
 
 SPARK_POD_NAME=$1
 DVRPODPREFIX="cohortfvt-spark"
-driverpod=`kubectl -n ${CLUSTER_NAMESPACE} get pods --no-headers -o custom-columns=":metadata.name" | grep ${DVRPODPREFIX} | grep driver`
-echo "Driver pod is: ${driverpod}"
-echo "Getting status of driver pod: ${driverpod}"
-status=`kubectl -n ${CLUSTER_NAMESPACE} get pod ${driverpod} --no-headers -o custom-columns=":status.phase"`
-echo "driver pod status is: ${status}"
+DRIVERPOD=`kubectl -n ${CLUSTER_NAMESPACE} get pods --no-headers -o custom-columns=":metadata.name" | grep ${DVRPODPREFIX} | grep driver`
+echo "Driver pod is: ${DRIVERPOD}"
+echo "Getting status of driver pod: ${DRIVERPOD}"
+status=`kubectl -n ${CLUSTER_NAMESPACE} get pod ${DRIVERPOD} --no-headers -o custom-columns=":status.phase"`
+echo "Driver pod status is: ${status}"
 count=1
 while ( [ "${status}" == "Pending" ] || [ "${status}" == "Running" ] ) && [ ${count} le 30 ] 
 do
-   echo "Polling again for the status of driver pod after a wait of 10 seconds"
+   echo "Polling again for the status of Driver pod after a wait of 10 seconds"
    sleep 10
-   status=`kubectl -n ${CLUSTER_NAMESPACE} get ${driverpod} --no-headers -o custom-columns=":status.phase"`
-   echo "driver pod status is: ${status} - count is : ${count}"
-   ${count}=${count}+1
+   status=`kubectl -n ${CLUSTER_NAMESPACE} get ${DRIVERPOD} --no-headers -o custom-columns=":status.phase"`
+   echo "Driver pod status is: ${status} - count is : ${count}"
+   count=$((count + 1))
 done
 
 if [ "${status}" != "Succeeded" ]; then 
@@ -29,7 +29,7 @@ if [ "${status}" != "Succeeded" ]; then
 fi
 
 echo "Executing spark-submit --status on engine-spark-fvt-test pod and writing out the status log to file sparksbmsts.txt file."
-kubectl -n ${CLUSTER_NAMESPACE} exec ${SPARK_POD_NAME} -- bash -c "/opt/spark/bin/spark-submit --status ${CLUSTER_NAMESPACE}:${driverpod} --master k8s://https://c105.us-east.containers.cloud.ibm.com:32327 2> /tmp/sparksbmsts.txt"
+kubectl -n ${CLUSTER_NAMESPACE} exec ${SPARK_POD_NAME} -- bash -c "/opt/spark/bin/spark-submit --status ${CLUSTER_NAMESPACE}:${DRIVERPOD} --master k8s://https://c105.us-east.containers.cloud.ibm.com:32327 2> /tmp/sparksbmsts.txt"
 #Copying the status log file from the ${SPARK_POD_NAME} filesystem to a local directory.
 kubectl -n ${CLUSTER_NAMESPACE} cp ${SPARK_POD_NAME}:/tmp/sparksbmsts.txt sparksbmsts.txt
 #Extracting the exit code from the spark-submit job status log
