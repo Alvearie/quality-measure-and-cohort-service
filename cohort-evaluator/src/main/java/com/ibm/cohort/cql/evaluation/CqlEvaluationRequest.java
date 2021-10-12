1f3916f5700667e1273e88fef43edd7f33d120df
matthew.sargent <matthew.sargent@ibm.com>
2021-10-12 17:08:08 -0400

    Get rid of mixed spacing
    Signed-off-by: matthew.sargent <matthew.sargent@ibm.com>

--- cohort-evaluator/src/main/java/com/ibm/cohort/cql/evaluation/CqlEvaluationRequest.java
+++ cohort-evaluator/src/main/java/com/ibm/cohort/cql/evaluation/CqlEvaluationRequest.java
@@ -23,56 +23,56 @@ import com.ibm.cohort.cql.evaluation.parameters.Parameter;
 import com.ibm.cohort.cql.library.CqlLibraryDescriptor;
 
 public class CqlEvaluationRequest {
-	@JsonIgnore
-	private Integer id;
+    @JsonIgnore
+    private Integer id;
     private CqlLibraryDescriptor descriptor;
-	private Set<CqlExpressionConfiguration> expressions;
-	@Valid
-	private Map<String,Parameter> parameters;
-	private String contextKey;
-	private String contextValue;
-	
-	public CqlEvaluationRequest() {
-	    super();
-	}
-	
-	public CqlEvaluationRequest(CqlEvaluationRequest other) {
-		this.setId(other.getId());
-	    this.setDescriptor(other.getDescriptor());
-	    this.setContextKey(other.getContextKey());
-	    this.setContextValue(other.getContextValue());
-	    if( other.getExpressions() != null ) {
-	        this.setExpressions(new HashSet<>(other.getExpressions()));    
-	    }
-	    if( other.getParameters() != null ) {
-	        this.setParameters(new HashMap<>(other.getParameters()));
-	    }
-	}
+    private Set<CqlExpressionConfiguration> expressions;
+    @Valid
+    private Map<String,Parameter> parameters;
+    private String contextKey;
+    private String contextValue;
+    
+    public CqlEvaluationRequest() {
+        super();
+    }
+    
+    public CqlEvaluationRequest(CqlEvaluationRequest other) {
+        this.setId(other.getId());
+        this.setDescriptor(other.getDescriptor());
+        this.setContextKey(other.getContextKey());
+        this.setContextValue(other.getContextValue());
+        if( other.getExpressions() != null ) {
+            this.setExpressions(new HashSet<>(other.getExpressions()));    
+        }
+        if( other.getParameters() != null ) {
+            this.setParameters(new HashMap<>(other.getParameters()));
+        }
+    }
 
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public CqlLibraryDescriptor getDescriptor() {
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public CqlLibraryDescriptor getDescriptor() {
         return descriptor;
     }
     public void setDescriptor(CqlLibraryDescriptor descriptor) {
         this.descriptor = descriptor;
     }
-	public Set<CqlExpressionConfiguration> getExpressions() {
-		return expressions;
-	}
-	public void setExpressions(Set<CqlExpressionConfiguration> expressions) {
-		this.expressions = expressions;
-	}
-	public Map<String, Parameter> getParameters() {
-		return parameters;
-	}
-	public void setParameters(Map<String, Parameter> parameters) {
-		this.parameters = parameters;
-	}
+    public Set<CqlExpressionConfiguration> getExpressions() {
+        return expressions;
+    }
+    public void setExpressions(Set<CqlExpressionConfiguration> expressions) {
+        this.expressions = expressions;
+    }
+    public Map<String, Parameter> getParameters() {
+        return parameters;
+    }
+    public void setParameters(Map<String, Parameter> parameters) {
+        this.parameters = parameters;
+    }
     public String getContextKey() {
         return contextKey;
     }
@@ -87,19 +87,19 @@ public class CqlEvaluationRequest {
     }
     
     public Set<String> getExpressionNames() {
-		return expressions.stream().map(CqlExpressionConfiguration::getName).collect(Collectors.toSet());
-	}
-	
-	public void setExpressionsByNames(Set<String> expressionNames) {
-		Set<CqlExpressionConfiguration> expressionConfigurations = new HashSet<>();
-		for (String expressionName : expressionNames) {
-			CqlExpressionConfiguration expressionConfiguration = new CqlExpressionConfiguration();
-			expressionConfiguration.setName(expressionName);
-			expressionConfigurations.add(expressionConfiguration);
-		}
-		
-		this.expressions = expressionConfigurations;
-	}
+        return expressions.stream().map(CqlExpressionConfiguration::getName).collect(Collectors.toSet());
+    }
+    
+    public void setExpressionsByNames(Set<String> expressionNames) {
+        Set<CqlExpressionConfiguration> expressionConfigurations = new HashSet<>();
+        for (String expressionName : expressionNames) {
+            CqlExpressionConfiguration expressionConfiguration = new CqlExpressionConfiguration();
+            expressionConfiguration.setName(expressionName);
+            expressionConfigurations.add(expressionConfiguration);
+        }
+        
+        this.expressions = expressionConfigurations;
+    }
     
     @Override
     public String toString() {
@@ -112,33 +112,33 @@ public class CqlEvaluationRequest {
                 .toString();
     }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
 
-		if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
-		CqlEvaluationRequest that = (CqlEvaluationRequest) o;
+        CqlEvaluationRequest that = (CqlEvaluationRequest) o;
 
-		return new EqualsBuilder()
-				.append(id, that.id)
-				.append(descriptor, that.descriptor)
-				.append(expressions, that.expressions)
-				.append(parameters, that.parameters)
-				.append(contextKey, that.contextKey)
-				.append(contextValue, that.contextValue)
-				.isEquals();
-	}
+        return new EqualsBuilder()
+                .append(id, that.id)
+                .append(descriptor, that.descriptor)
+                .append(expressions, that.expressions)
+                .append(parameters, that.parameters)
+                .append(contextKey, that.contextKey)
+                .append(contextValue, that.contextValue)
+                .isEquals();
+    }
 
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder(17, 37)
-				.append(id)
-				.append(descriptor)
-				.append(expressions)
-				.append(parameters)
-				.append(contextKey)
-				.append(contextValue)
-				.toHashCode();
-	}
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(descriptor)
+                .append(expressions)
+                .append(parameters)
+                .append(contextKey)
+                .append(contextValue)
+                .toHashCode();
+    }
 }
