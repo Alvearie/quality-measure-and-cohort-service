c86bad10abc0cba3b820eab18148acc24ec87772
Timothy Bishop <72571312+tabishop@users.noreply.github.com>
2021-11-17 12:21:26 -0600

    Index codes in valuesets for faster lookups (#205)
    * Index codes in valuesets for faster lookups
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    
    * Add in --nobest to work around toolchain issues
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    
    * Code review comments and fix missing code system version on
    valuesetimporter
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    
    * Add missing dependency
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    
    * Fix bad import, code comment
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    
    * removed unneeded test line
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    
    * Tweak checking when no codeSystem is provided
    
    Signed-off-by: TimBishop <tabishop@us.ibm.com>
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

---
+++ cohort-util/src/main/java/com/ibm/cohort/valueset/CodeSystemKey.java
@@ -0,0 +1,64 @@
+/*
+ * (C) Copyright IBM Corp. 2021
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.cohort.valueset;
+
+/*
+ * This is a simple class to allow a codeSystem and it's version to be used as a key in a hashMap
+ */
+class CodeSystemKey {
+
+	public String codeSystem;
+	public String codeSystemVersion;
+
+	public CodeSystemKey(String codeSystem, String codeSystemVersion) {
+		this.codeSystem = codeSystem;
+		this.codeSystemVersion = codeSystemVersion;
+	}
+
+	public String getCodeSystem() {
+		return codeSystem;
+	}
+
+	public String getCodeSystemVersion() {
+		return codeSystemVersion;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		CodeSystemKey key = (CodeSystemKey) o;
+		if (codeSystem != null ? !codeSystem.equals(key.codeSystem) : key.codeSystem != null) {
+			return false;
+		}
+
+		if (codeSystemVersion != null ? !codeSystemVersion.equals(key.codeSystemVersion)
+				: key.codeSystemVersion != null) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = codeSystem != null ? codeSystem.hashCode() : 0;
+		result = 31 * result + (codeSystemVersion != null ? codeSystemVersion.hashCode() : 0);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + codeSystem + ", " + codeSystemVersion + "]";
+	}
+}
