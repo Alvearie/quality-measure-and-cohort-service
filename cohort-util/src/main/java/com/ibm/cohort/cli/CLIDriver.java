4d23d885bcba42191f8a44d841c1e30f45f6e406
matthew.sargent <matthew.sargent@ibm.com>
2022-01-12 16:27:51 -0500

    update pom; ignore driver class for tests
    Signed-off-by: matthew.sargent <matthew.sargent@ibm.com>

--- cohort-util/src/main/java/com/ibm/cohort/cli/CLIDriver.java
+++ cohort-util/src/main/java/com/ibm/cohort/cli/CLIDriver.java
@@ -5,8 +5,15 @@ import java.util.Collections;
 import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.ibm.cohort.annotations.Generated;
+
+@Generated
 public class CLIDriver {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CLIDriver.class);
+	
 	public static void main(String[] args) {
 		CLIDriver cliDriver = new CLIDriver();
 		cliDriver.runProgram(args);
@@ -23,7 +30,7 @@ public class CLIDriver {
 		catch (UnsupportedOperationException e) {
 			Set<String> validCommands = getValidCommands();
 			if (CollectionUtils.isNotEmpty(validCommands)) {
-				System.err.println("Valid commands are: " + String.join(", ", validCommands));
+				LOGGER.error("Valid commands are: {}", String.join(", ", validCommands));
 			}
 			throw e;
 		}
