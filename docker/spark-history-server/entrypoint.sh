329d7ac1e85f948f499aa4f5263ade53ba947b27
TimBishop <tabishop@us.ibm.com>
2022-01-21 17:01:05 -0600

    Try using sparkHistoryOpts
    Signed-off-by: TimBishop <tabishop@us.ibm.com>

--- docker/spark-history-server/entrypoint.sh
+++ docker/spark-history-server/entrypoint.sh
@@ -10,10 +10,6 @@ export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS \
     -Dspark.history.fs.logDirectory=s3a://$AWS_BUCKET$AWS_LOG_PATH \
     -Dspark.hadoop.fs.s3a.endpoint=$AWS_ENDPOINT \
     -Dspark.hadoop.fs.s3a.access.key=$AWS_ACCESS_KEY \
-    -Dspark.hadoop.fs.s3a.secret.key=$AWS_SECRET_KEY \
-    -Dspark.history.fs.cleaner.enabled=$SPARK_HISTORY_FS_CLEANER_ENABLED \
-    -Dspark.history.fs.cleaner.interval=$SPARK_HISTORY_FS_CLEANER_INTERVAL \ 
-    -Dspark.history.fs.cleaner.maxAge=$SPARK_HISTORY_FS_CLEANER_MAXAGE \
-    -Dspark.history.fs.cleaner.maxNum=$SPARK_HISTORY_FS_CLEANER_MAXNUM;"
+    -Dspark.hadoop.fs.s3a.secret.key=$AWS_SECRET_KEY;"
 
 /opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer;
