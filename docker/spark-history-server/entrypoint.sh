a123e47189015947842e9317db1e3159e25b789a
TimBishop <tabishop@us.ibm.com>
2022-01-24 08:46:56 -0600

    fix bad semicolon placement
    Signed-off-by: TimBishop <tabishop@us.ibm.com>

--- docker/spark-history-server/entrypoint.sh
+++ docker/spark-history-server/entrypoint.sh
@@ -10,6 +10,6 @@ export SPARK_HISTORY_OPTS="$SPARK_HISTORY_OPTS \
     -Dspark.history.fs.logDirectory=s3a://$AWS_BUCKET$AWS_LOG_PATH \
     -Dspark.hadoop.fs.s3a.endpoint=$AWS_ENDPOINT \
     -Dspark.hadoop.fs.s3a.access.key=$AWS_ACCESS_KEY \
-    -Dspark.hadoop.fs.s3a.secret.key=$AWS_SECRET_KEY;"
+    -Dspark.hadoop.fs.s3a.secret.key=$AWS_SECRET_KEY";
 
 /opt/spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer;
