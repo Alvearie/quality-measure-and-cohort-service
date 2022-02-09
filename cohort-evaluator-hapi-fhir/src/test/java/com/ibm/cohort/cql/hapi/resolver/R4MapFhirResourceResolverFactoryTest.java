d4faddae3cdbddbcbe49fdea1f9150a70854a02c
David Kwasny <dkwasny@us.ibm.com>
2022-02-09 12:06:14 -0500

    Adding comments
    Explaining a subtle difference between how the "subsub" folder test case differs between zip and directory file sources.
    
    Signed-off-by: David Kwasny <dkwasny@us.ibm.com>

--- cohort-evaluator-hapi-fhir/src/test/java/com/ibm/cohort/cql/hapi/resolver/R4MapFhirResourceResolverFactoryTest.java
+++ cohort-evaluator-hapi-fhir/src/test/java/com/ibm/cohort/cql/hapi/resolver/R4MapFhirResourceResolverFactoryTest.java
@@ -80,6 +80,13 @@ public class R4MapFhirResourceResolverFactoryTest {
     @Test
     public void fromDirectory_searchPath_subSubFolder() throws IOException {
         Path directory = Paths.get("src/test/resources/fhir/file-test");
+        /* Note the use of `Paths.get()` for the searchPath.
+         * Java will change the path separator to the OS default when resolving down to a string.
+         * This allows the test to run on Unix and Windows machines without any code changes.
+         *
+         * See `fromZipFile_searchPath_subSubFolder()` for how we handle this
+         * usecase with zip files.
+         */
         Path searchPath = Paths.get("sub-folder", "subsub-folder");
         FhirResourceResolver<Library> resolver = factory.fromDirectory(directory, searchPath.toString());
 
@@ -120,6 +127,15 @@ public class R4MapFhirResourceResolverFactoryTest {
     @Test
     public void fromZipFile_searchPath_subSubFolder() throws IOException {
         Path directory = Paths.get("src/test/resources/fhir/file-test/zipped-libraries.zip");
+        /* Note the hard-coded use of `/` here as the searchPath separator.
+         * Even though Unix and Windows have different path separators, both
+         * OSes will read the paths from inside the zip file with a `/`.
+         * I believe this is tied to the fact that the zip file was created
+         * in a Mac (Unix) environment.
+         *
+         * See `fromDirectory_searchPath_subSubFolder()` for how we handle
+         * this usecase with directories.
+         */
         FhirResourceResolver<Library> resolver = factory.fromZipFile(directory, "sub-folder/subsub-folder");
 
         assertIdsArePresent(
